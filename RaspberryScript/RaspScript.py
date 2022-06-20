71acb271cca903319740d3499ab31bd4f4fb5a62
tiago-miranda-712 <107780260+tiago-miranda-712@users.noreply.github.com>
2022-06-20 17:43:47 -0300

    Add files via upload

---
+++ RaspberryScript/RaspScript.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+##importa as bibliotecas para o script
+import RPI.GPIO as GPIO
+import time
+import serial
+import json
+import requests
+
+##todo codigo abaixo referece ao setup
+##molde da api de medicao do sistem
+apiDados = {
+    "tempoUnix" : unix_t : int,
+    "potencia" : Sva : float,
+    "corrente" : Amps : float,
+    "tensao" : Volt : float,
+    "estado" : S_Rele : bool
+    }
+
+##loop
+while(True):
+
+
+def leituraUpload():
+
+
